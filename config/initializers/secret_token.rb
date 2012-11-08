cf366c616f83f7ba23d84be296f3ba11a28be48d
Fred Schoeneman <fred.schoeneman@gmail.com>
2012-11-08 15:51:11 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Anydiver::Application.config.secret_token = '03b0c7b1dd3c48fd88ed9b75cef27c5c59b13ae52f954c72a37f8c69a977f48b6fc0a769c1ad5ef2205247d44a4112fd367a1974d775d41995e5a08bf2488674'
